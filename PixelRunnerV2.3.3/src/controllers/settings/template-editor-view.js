dcb0d0ae2f2f3416953e022e62fb1d03d0a631ce
XIAOTsune <1048855084@qq.com>
2026-04-14 19:50:10 +0800

    2.4.1main

--- PixelRunnerV2.3.3/src/controllers/settings/template-editor-view.js
+++
@@ -1,9 +0,0 @@
-function renderTemplateLengthHint(hintEl, viewModel) {
-  if (!hintEl || !viewModel) return;
-  hintEl.textContent = String(viewModel.text || "");
-  hintEl.style.color = String(viewModel.color || "");
-}
-
-module.exports = {
-  renderTemplateLengthHint
-};
--- PixelRunnerV2.3.3/src/services/ps.js
+++
@@ -1,9 +0,0 @@
-const capture = require("./ps/capture");
-const place = require("./ps/place");
-const tools = require("./ps/tools");
-
-module.exports = {
-  ...capture,
-  ...place,
-  ...tools
-};
